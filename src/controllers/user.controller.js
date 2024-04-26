5037c1debc21e1812ae3fc86c6f9503d2408729f
isaac192063 <ichinchillac@ufpso.edu.co>
2024-04-25 22:15:43 -0500

    configuracion de railway

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,158 +1,155 @@
-const prisma = require('../config/prisma')
-const bcript = require('bcrypt')
-const jwt = require('jsonwebtoken');
-const { key } = require('../config/key');
+const prisma = require("../config/prisma");
+const bcript = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { key } = require("../config/key");
 
 module.exports = {
-    async getAllUsers(req, res){
-        try {
-            const users = await prisma.user.findMany();
-            res.status(200).json({
-                "success": true,
-                "data": users
-            });
-        } catch (error) {
-            console.log(error)
-            return res.status(401).json({
-                "success": false,
-                "message": "Error en el sistema"
-            })
-        }
-    }, 
-    async createUser(req,res){
-        try{
-            const user = req.body
-
-            const file = req.file
-            user.image = file.filename
-
-            const password_encrypted = await bcript.hash(user.password, 10);
-            user.password = password_encrypted
-
-            const rol = await prisma.rol.findFirst({where:{id_rol: parseInt(user.rol)}})
-            console.log(rol);
-            if(!rol){
-                return res.status(400).json({
-                    "success": false,
-                    "message": "bad request"
-                })
-            }
-            delete user.rol
-            const newUser = await prisma.user.create({
-                data: user,
-            })
-
-            const userRol = await prisma.roleUser.create({
-                data: {idRol: rol.id_rol, idUser: newUser.id_user}
-            })
-
-            user.rol = rol.name
-
-            res.status(201).json({
-                "success": true,
-                data: user
-            })
-        
-        }catch(error){
-            console.log(error)
-            return res.status(401).json({
-                "success": false,
-                "message": "Error en el sistema"
-            })
-        }
-            
-    },
-    async loginUser(req,res){
-        try {
-          const {password, email} = req.body
-    
-          const userEmail = await prisma.user.findFirst({where: {email}})
-          
-          if(!userEmail){
-            return res.status(401).json({
-                "success": false,
-                "message": "Empledo no fue encontrado"
-            })
-          }
-    
-          const userComparation = await bcript.compare(password, userEmail.password);
-
-          
-          if(!userComparation){
-              return res.status(400).json({
-                  "success": false,
-                  "message": "Contraseña incorrecta",
-                })
-            }
-            
-            const userRol = await prisma.roleUser.findFirst({where: {idUser: userEmail.id_user},include:{rol: true}})
-            const token = jwt.sign({id: userEmail.id_user, "rol": userRol.rol.name},key)
-            
-            return res.status(203).json({
-                "success": true,
-                "message": "usuario autenticado",
-                "token":token,
-          })
-    
-        } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                "success": false,
-                "message": "Error en el servidor"
-            })
-        }
-    },
-    async getUserById(req, res){
-
-        try {
-            const id_user = parseInt(req.params.id);
-            const user = await prisma.user.findFirst({where:{id_user}})
-        
-            if(!user){
-                return res.status(203).json({
-                    "success": false,
-                    "message": "usuario no encontrado",
-              })
-            }
-            
-            return res.status(200).json({
-                "success": true,
-                "message": "usuario encontrado",
-                "data": user
-            })
-            
-        } catch (error) {
-            return res.status(203).json({
-                "success": false,
-                "message": "Error en el servidor",
-          })
-        }
-    
-    },
-    async getAllEmployes(req, res){
-        try {
-            const employes = prisma.user.findMany({where: {
-                rolUser:{
-                    some:{
-                        idRol: 1
-                    }
-                }
-            }})
-            const listEmploye = new Array();
-            (await employes).forEach(employe=>{
-                listEmploye.push(employe);
-            })
-            res.status(200).json({
-                "success": true, 
-                "data": listEmploye
-            });
-            
-        } catch (error) {
-            return res.status(203).json({
-                "success": false,
-                "message": "Error en el servidor",
-          })
-        }
+  async getAllUsers(req, res) {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json({
+        success: true,
+        data: users,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({
+        success: false,
+        message: "Error en el sistema",
+      });
     }
+  },
+  async createUser(req, res) {
+    try {
+      const user = req.body;
 
-}
+      const file = req.file;
+      user.image = file.filename;
+
+      const password_encrypted = await bcript.hash(user.password, 10);
+      user.password = password_encrypted;
+
+      const rol = await prisma.rol.findFirst({
+        where: { id_rol: parseInt(user.rol) },
+      });
+      console.log(rol);
+      if (!rol) {
+        return res.status(400).json({
+          success: false,
+          message: "bad request",
+        });
+      }
+      const aux_rol = user.rol;
+      delete user.rol;
+
+      user.idRol = parseInt(aux_rol);
+
+      const newUser = await prisma.user.create({
+        data: user,
+      });
+
+      res.status(201).json({
+        success: true,
+        data: newUser,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({
+        success: false,
+        message: "Error en el sistema",
+      });
+    }
+  },
+  async loginUser(req, res) {
+    try {
+      const { password, email } = req.body;
+
+      const userEmail = await prisma.user.findFirst({ where: { email } });
+
+      if (!userEmail) {
+        return res.status(401).json({
+          success: false,
+          message: "Empledo no fue encontrado",
+        });
+      }
+
+      const userComparation = await bcript.compare(
+        password,
+        userEmail.password
+      );
+
+      if (!userComparation) {
+        return res.status(400).json({
+          success: false,
+          message: "Contraseña incorrecta",
+        });
+      }
+
+      const user = await prisma.user.findFirst({
+        where: { id_user: userEmail.id_user },
+        include: { rolUser: true },
+      });
+      const rol = await prisma.rol.findFirst({where:{id_rol: user.idRol}})
+      console.log(rol.name);
+      const token = jwt.sign({ id: userEmail.id_user, rol: rol.name }, key);
+
+      return res.status(203).json({
+        success: true,
+        message: "usuario autenticado",
+        token: token,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Error en el servidor",
+      });
+    }
+  },
+  async getUserById(req, res) {
+    try {
+      const id_user = parseInt(req.params.id);
+      const user = await prisma.user.findFirst({ where: { id_user } });
+
+      if (!user) {
+        return res.status(203).json({
+          success: false,
+          message: "usuario no encontrado",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "usuario encontrado",
+        data: user,
+      });
+    } catch (error) {
+      return res.status(203).json({
+        success: false,
+        message: "Error en el servidor",
+      });
+    }
+  },
+  async getAllEmployes(req, res) {
+    try {
+      const employes = prisma.user.findMany({
+        where: {
+          idRol: 1
+        },
+      });
+      const listEmploye = new Array();
+      (await employes).forEach((employe) => {
+        listEmploye.push(employe);
+      });
+      res.status(200).json({
+        success: true,
+        data: listEmploye,
+      });
+    } catch (error) {
+      return res.status(203).json({
+        success: false,
+        message: "Error en el servidor",
+      });
+    }
+  },
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,41 @@
-const {Router} = require('express')
-const employeeController = require('../controllers/user.controller')
-const prisma = require('../config/prisma')
-const multer = require('../config/multer')
+const { Router } = require("express");
+const employeeController = require("../controllers/user.controller");
+const prisma = require("../config/prisma");
+const multer = require("../config/multer");
 
-const routerUser = Router()
+const routerUser = Router();
 
+routerUser.get("/api/user", employeeController.getAllUsers);
+routerUser.post(
+  "/api/user",
+  multer.single("image"),
+  employeeController.createUser
+);
+routerUser.post("/api/user/login", employeeController.loginUser);
+routerUser.get("/api/user/emp", employeeController.getAllEmployes);
+routerUser.get("/api/user/:id", employeeController.getUserById);
+routerUser.put("/api/user/:id", async (req, res) => {
+  try {
+    const newUser = req.body;
+    const id = req.params.id;
 
-routerUser.get("/api/user",  employeeController.getAllUsers)
-routerUser.post("/api/user", multer.single('image'), employeeController.createUser)
-routerUser.post("/api/user/login", employeeController.loginUser)
-routerUser.get("/api/user/emp", employeeController.getAllEmployes)
-routerUser.get("/api/user/:id", employeeController.getUserById)
-module.exports = routerUser
+    const updateEmployee = await prisma.user.update({
+      where: {
+        id_user: parseInt(id),
+      },
+      data: {
+        name: newUser.name,
+        last_name: newUser.last_name,
+        phone_number: newUser.phone_number,
+      },
+    });
+
+    res.status(203).json(updateEmployee);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+        "message": "error"
+    })
+  }
+});
+module.exports = routerUser;
